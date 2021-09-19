f570d1aaf74fb2a87a14160a45b9b073ae11fd04
Jose Martin Salcedo <salcedo.nerv@gmail.com>
2021-09-19 18:26:45 -0500

    add correct logic for search bar

--- Comida/Buscador/ViewController.swift
+++ Comida/Buscador/ViewController.swift
@@ -8,136 +8,65 @@
 import UIKit
 
 class ViewController: UIViewController {
-  
-  @IBOutlet weak var tableView: UITableView!
-  @IBOutlet weak var emptyLabel: UILabel!
-  
-  /// definition of search bar
-  let searchController = UISearchController(searchResultsController: nil)
-  var isSearchBarEmpty: Bool {
-    return searchController.searchBar.text?.isEmpty ?? true
-  }
-  
-  var isFiltering: Bool {
-    return searchController.isActive && !isSearchBarEmpty
-  }
-  
-  var food = [Food]()
-  var filteredFood: [Food] = []
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    tableView.isHidden = true
-    emptyLabel.isHidden = true
-    tableView.delegate = self
-    tableView.dataSource = self
-    tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "food")
-    setupConfigSearchBar()
-    emptyLabel.text = "No hay alimentos"
-  }
-  
-  /// Condig the search bar
-  private func setupConfigSearchBar() {
-    searchController.searchBar.delegate = self
-    searchController.searchResultsUpdater = self
-    searchController.obscuresBackgroundDuringPresentation = false
-    searchController.searchBar.placeholder = "Search Food"
-    navigationItem.searchController = searchController
-    definesPresentationContext = true
-  }
-  
-  func filterContentForSearchText(_ searchText: String) {
-    ServiceFoodProvider.shared.getListFood(textSearch: searchText) { (data) in
-      self.filteredFood = data
-      self.emptyLabel.isHidden = true
-      self.tableView.isHidden = false
-      self.tableView.reloadData()
-    } failure: { (error) in
-      self.tableView.isHidden = true
-      self.emptyLabel.isHidden = false
-      self.emptyLabel.text = "No se encontraron resultados"
-      print(error.debugDescription)
-    }
-  }
-
-}
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    if isFiltering {
-      return filteredFood.count
-    }
-    return food.count
-  }
-  
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    guard let cell = tableView.dequeueReusableCell(withIdentifier: "food", for: indexPath) as? TableViewCell else { return UITableViewCell() }
-    let nameFood = isFiltering ? filteredFood[indexPath.row] : food[indexPath.row]
-    cell.titleOfFood.text = nameFood.strMeal ?? ""
-    cell.categoryOfFood.text = nameFood.strCategory ?? ""
-    cell.imagenView.downloaded(from: nameFood.strMealThumb ?? "")
-    cell.selectionStyle = .none
-    return cell
-  }
-  
-  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     
-    let vc = DetailViewController(nibName: "DetailViewController", bundle: nil)
-    vc.idFood = "si seleciono"
-//    navigationController?.pushViewController(vc, animated: true)
-  }
-  
-  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    switch segue.identifier {
-    case "DetailViewController":
-      let vc = segue.destination as! DetailViewController
-      vc.idFood = "la comida es"
-    default:
-      break
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var emptyLabel: UILabel!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+//    Data for tableView
+    let data = ["New York, NY", "Los Angeles, CA", "Chicago, IL", "Houston, TX",
+            "Philadelphia, PA", "Phoenix, AZ", "San Diego, CA", "San Antonio, TX",
+            "Dallas, TX", "Detroit, MI", "San Jose, CA", "Indianapolis, IN",
+            "Jacksonville, FL", "San Francisco, CA", "Columbus, OH", "Austin, TX",
+            "Memphis, TN", "Baltimore, MD", "Charlotte, ND", "Fort Worth, TX"]
+    
+    var filteredData: [String]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "food")
+        setupConfigSearchBar()
+        filteredData = data
+        emptyLabel.isHidden = true
+        emptyLabel.text = "No hay alimentos"
     }
-  }
+    
+    /// Condig the search bar
+    private func setupConfigSearchBar() {
+        searchBar.delegate = self
+    }
+    
 }
-extension ViewController: UISearchResultsUpdating {
-  func updateSearchResults(for searchController: UISearchController) {
-//    TODO: implementar para buscar en automatico
-  }
+// MARK: - UITableViewDelegate, UITableViewDataSource
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "food", for: indexPath) as? TableViewCell else { return UITableViewCell() }
+        cell.titleOfFood.text = filteredData[indexPath.row]
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "DetailViewController" {
+            if let destination = segue.destination as? DetailViewController {
+                guard let path = tableView.indexPathForSelectedRow else  { return }
+                let cell = tableView.cellForRow(at: path)
+                destination.idFood = cell?.textLabel?.text ?? "hola mundo"
+            }
+        }
+    }
 }
+// MARK: - UISearchBarDelegate
 extension ViewController: UISearchBarDelegate {
-
-  func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-    guard let text = searchBar.text else { return }
-    filterContentForSearchText(text)
-  }
-  
-  func searchBarShouldBeginEditing(_: UISearchBar) -> Bool {
-    tableView.isHidden = true
-    emptyLabel.isHidden = true
-    return true
-  }
-
-  func searchBarCancelButtonClicked(_: UISearchBar) {
-    tableView.isHidden = true
-    emptyLabel.isHidden = true
-  }
-
-}
-extension UIImageView {
-  func downloaded(from url: URL, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
-    contentMode = mode
-    URLSession.shared.dataTask(with: url) { data, response, error in
-      guard
-        let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
-        let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
-        let data = data, error == nil,
-        let image = UIImage(data: data)
-      else { return }
-      DispatchQueue.main.async() { [weak self] in
-        self?.image = image
-      }
-    }.resume()
-  }
-  
-  func downloaded(from link: String, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
-    guard let url = URL(string: link) else { return }
-    downloaded(from: url, contentMode: mode)
-  }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredData = searchText.isEmpty ? data : data.filter { (item: String) -> Bool in
+            return item.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+        }
+        tableView.reloadData()
+    }
 }
